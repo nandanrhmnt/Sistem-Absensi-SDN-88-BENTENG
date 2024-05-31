e0234c61c6872131a696d7d2b6365ae8e961c3ca
Nanda Nurahmanita <95604911+nandanrhmnt@users.noreply.github.com>
2024-05-31 09:26:37 +0700

    Update absensi_sdn88.sql
    update handphone

--- mysql_db/absensi_sdn88.sql
+++ mysql_db/absensi_sdn88.sql
@@ -109,20 +109,20 @@ CREATE TABLE `tb_pegawai` (
 
 INSERT INTO `tb_pegawai` (`id_pegawai`, `NIP`, `keterangan`, `Nama`, `jenis_kelamin`, `No_hp`, `created_at`, `updated_at`) VALUES
 (16, '196705251988032000', 'guru', 'Helendra, M.Pd.', 'Perempuan', '085288507676', NULL, '2024-05-25 11:57:51'),
-(17, '196602091989121001', 'guru', 'Suyoto, S.Pd.', 'Laki-laki', '081762830881', NULL, NULL),
-(18, '196805121994052001', 'guru', 'Nuryati Siallagan, S.Pd.', 'Perempuan', '080986246788', NULL, NULL),
-(19, '196807011994032006', 'guru', 'Indarmawati, S.Pd.', 'Perempuan', '085298734224', NULL, NULL),
-(20, '196906171997032002', 'guru', 'Rosdalina, S.Pd.', 'Perempuan', '085876434578', NULL, NULL),
-(21, '196908141992062001', 'guru', 'Poniem, S.Pd.', 'Perempuan', '085287653256', NULL, NULL),
-(22, '197105201996062001', 'guru', 'Purnamawati, S.Pd.', 'Perempuan', '081357897458', NULL, NULL),
-(23, '198203052014022002', 'guru', 'Fitri Handayani, S.Pd.', 'Perempuan', '088896421122', NULL, NULL),
-(24, '198711142019032002', 'guru', 'Endah Permana Sari, S.Pd.I', 'Perempuan', '081346909952', NULL, NULL),
-(25, '', 'honorer', 'Tika Oktaviani, S.Pd.', 'Perempuan', '081346909952', NULL, NULL),
-(26, '', 'honorer', 'Agung Setia Budi, S.Pd.', 'Laki-laki', '081346909952', NULL, NULL),
-(27, '', 'honorer', 'Dewi Kurniati, S.E.', 'Perempuan', '081346909952', NULL, NULL),
-(28, '', 'honorer', 'Sukamto', 'Laki-laki', '081346909952', NULL, NULL),
-(29, '', 'honorer', 'Bainah', 'Perempuan', '081346909952', NULL, NULL),
-(30, NULL, 'honorer', 'Nur Hasanah', 'Perempuan', '081346909952', NULL, '2024-05-25 11:58:22');
+(17, '196602091989121001', 'guru', 'Suyoto, S.Pd.', 'Laki-laki', '085267491054', NULL, NULL),
+(18, '196805121994052001', 'guru', 'Nuryati Siallagan, S.Pd.', 'Perempuan', '085368375149', NULL, NULL),
+(19, '196807011994032006', 'guru', 'Indarmawati, S.Pd.', 'Perempuan', '081210485860', NULL, NULL),
+(20, '196906171997032002', 'guru', 'Rosdalina, S.Pd.', 'Perempuan', '082289583162', NULL, NULL),
+(21, '196908141992062001', 'guru', 'Poniem, S.Pd.', 'Perempuan', '082175673369', NULL, NULL),
+(22, '197105201996062001', 'guru', 'Purnamawati, S.Pd.', 'Perempuan', '089507961451', NULL, NULL),
+(23, '198203052014022002', 'guru', 'Fitri Handayani, S.Pd.', 'Perempuan', '085286185708', NULL, NULL),
+(24, '198711142019032002', 'guru', 'Endah Permana Sari, S.Pd.I', 'Perempuan', '085381303300', NULL, NULL),
+(25, '', 'honorer', 'Tika Oktaviani, S.Pd.', 'Perempuan', '081366365929', NULL, NULL),
+(26, '', 'honorer', 'Agung Setia Budi, S.Pd.', 'Laki-laki', '082281728813', NULL, NULL),
+(27, '', 'honorer', 'Dewi Kurniati, S.E.', 'Perempuan', '082306605617', NULL, NULL),
+(28, '', 'honorer', 'Sukamto', 'Laki-laki', '082384609347', NULL, NULL),
+(29, '', 'honorer', 'Bainah', 'Perempuan', '082384609347', NULL, NULL),
+(30, NULL, 'honorer', 'Nur Hasanah', 'Perempuan', '083176801924', NULL, '2024-05-25 11:58:22');
 
 -- --------------------------------------------------------
 
